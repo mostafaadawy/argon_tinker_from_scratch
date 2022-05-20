1ee340aa5ca185c225af3ab954e248c10d410ea2
mostafaadawy <mostafa_adawy@ymail.com>
2022-05-20 12:51:40 +0200

    adding lang

---
+++ Modules/Accounting/Resources/lang/accounting.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    'sidebar' => [
+                    'main'=>'Accounting',
+                    'codesettings'=>'Code Settings',
+                    'codesettings_reven'=>'Revenue Code Settings',
+                    'codesettings_expen'=>'Expenses Code Settings',
+                    'dailyrecord'=>'Daily Record',
+                    'dailyrecord_reven'=>'Revenue Daily Records',
+                    'dailyrecord_expen'=>'Expenses Daily Records',
+                    'report'=> 'Report',
+                    'budgetterms'=> 'Budget Terms'
+    ],
+    'codesettings'=> 'Code Settings',
+    'revenue' => 'Revenue',
+    'items'=> 'Items',
+    'terms'=>'Terms',
+    'misc'=> 'Miscellaneous',
+    'expenses'=> 'Expenses',
+    'budgetTerms'=> 'Budget Terms',
+    'name'=>'Name',
+    'totalCredit'=>'Total Credit',
+    'totalDept'=> 'Total Debt',
+    'total'=> 'Total',
+    'code'=>'Code',
+    'subTotal'=>'Sub Total',
+    'details'=>'Details',
+    'type'=>'Type',
+    'account'=> 'Account',
+    'report'=>'Report'
+];
